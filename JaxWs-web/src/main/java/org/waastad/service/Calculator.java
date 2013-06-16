8911fb8332cc4453e939ac6d1f6f0b9430d71d24
Helge Waastad <helge.waastad@datametrix.no>
2013-06-16 22:13:50 +0200

    still tomee testing

--- JaxWs-web/src/main/java/org/waastad/service/Calculator.java
+++ JaxWs-web/src/main/java/org/waastad/service/Calculator.java
@@ -8,8 +8,14 @@ import javax.annotation.Resource;
 import javax.annotation.security.DeclareRoles;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
 import javax.jws.WebService;
 import javax.xml.ws.WebServiceContext;
+import no.datametrix.smartguest.ejb.model.EventLogMessage;
+import no.datametrix.smartguest.ejb.qualifier.EventLog;
+import no.datametrix.smartguest.ejb.service.AdminService;
+import no.datametrix.smartguest.ejb.service.RegisterService;
 import org.waastad.ejb.BusinessBean;
 
 /**
@@ -28,6 +34,13 @@ public class Calculator implements CalculatorWs {
     private BusinessBean businessBean;
     @Resource
     WebServiceContext wsContext;
+    @EJB
+    private RegisterService registerService;
+    @EJB
+    private AdminService adminService;
+    @Inject
+    @EventLog
+    private Event<EventLogMessage> eventLog;
 
     @Override
     //@RolesAllowed({"SuperAdminGroup", "ExternalAdminGroup"})
